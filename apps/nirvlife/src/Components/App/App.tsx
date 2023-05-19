08dabc3fa19e442f5077bf561f1c2534dec363ed
poop <noahedwardhall@gmail.com>
2023-05-19 07:01:00 -0500

    start with the commenting

--- apps/nirvlife/src/Components/App/App.tsx
+++ apps/nirvlife/src/Components/App/App.tsx
@@ -6,30 +6,31 @@ import { AppHeader } from "Components";
 import { APP_KEY } from "../../data/constants";
 import { AppTheme } from "Theme";
 
-export type AppProps = object;
-export class App extends Component<AppProps, Record<string, unknown>> {
-  componentDidUpdate(
-    prevProps: Readonly<AppProps>,
-    prevState: Readonly<Record<string, unknown>>,
-    snapshot?: unknown
-  ): void {
-    console.info(`${APP_KEY} updated`);
-  }
+export const App = () => <div>I am an app</div>;
+// export type AppProps = object;
+// export class App extends Component<AppProps, Record<string, unknown>> {
+//   componentDidUpdate(
+//     prevProps: Readonly<AppProps>,
+//     prevState: Readonly<Record<string, unknown>>,
+//     snapshot?: unknown
+//   ): void {
+//     console.info(`${APP_KEY} updated`);
+//   }
 
-  render() {
-    return (
-      <ThemeProvider theme={AppTheme}>
-        <CssBaseline key="baseline" />
-        <AppHeader key="header" />
-        <Container
-          disableGutters // using padding isntead
-          id="app-container"
-          key="container"
-        >
-          <Outlet />
-        </Container>
-        <ScrollRestoration key="scroll" />
-      </ThemeProvider>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <ThemeProvider theme={AppTheme}>
+//         <CssBaseline key="baseline" />
+//         <AppHeader key="header" />
+//         <Container
+//           disableGutters // using padding isntead
+//           id="app-container"
+//           key="container"
+//         >
+//           <Outlet />
+//         </Container>
+//         <ScrollRestoration key="scroll" />
+//       </ThemeProvider>
+//     );
+//   }
+// }
