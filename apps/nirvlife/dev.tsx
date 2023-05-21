43ba59c962d25f0764229cf5e33579f24f481420
poop <noahedwardhall@gmail.com>
2023-05-21 18:19:18 -0500

    async dev.tsx

--- apps/nirvlife/dev.tsx
+++ apps/nirvlife/dev.tsx
@@ -1,7 +1,6 @@
 import * as path from "path";
-import { statSync } from "fs";
 import type { ServeOptions } from "bun";
-import { rm } from "node:fs/promises";
+import { rm, stat } from "node:fs/promises";
 
 const PROJECT_ROOT = import.meta.dir;
 const PUBLIC_DIR = path.resolve(PROJECT_ROOT, "public");
@@ -22,18 +21,18 @@ await rm(BUILD_DIR, { force: true, recursive: true }).then(() =>
     })
 );
 
-function serveFromDir(config: {
+async function serveFromDir(config: {
   directory: string;
   path: string;
-}): Response | null {
+}): Promise<Response | null> {
   let basePath = path.join(config.directory, config.path);
   const suffixes = ["", ".html", "index.html"];
 
   for (const suffix of suffixes) {
     try {
       const pathWithSuffix = path.join(basePath, suffix);
-      const stat = statSync(pathWithSuffix);
-      if (stat && stat.isFile()) {
+      const fd = await stat(pathWithSuffix);
+      if (fd && fd.isFile()) {
         return new Response(Bun.file(pathWithSuffix));
       }
     } catch (err) {}
@@ -43,20 +42,23 @@ function serveFromDir(config: {
 }
 
 export default {
-  fetch(request) {
+  async fetch(request) {
     let reqPath = new URL(request.url).pathname;
     console.log(request.method, reqPath);
     if (reqPath === "/") reqPath = "/index.html";
 
     // check public
-    const publicResponse = serveFromDir({
+    const publicResponse = await serveFromDir({
       directory: PUBLIC_DIR,
       path: reqPath,
     });
     if (publicResponse) return publicResponse;
 
     // check /.build
-    const buildResponse = serveFromDir({ directory: BUILD_DIR, path: reqPath });
+    const buildResponse = await serveFromDir({
+      directory: BUILD_DIR,
+      path: reqPath,
+    });
     if (buildResponse) return buildResponse;
 
     return new Response("File not found", {
