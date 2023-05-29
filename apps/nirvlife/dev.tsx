413913ef4fe161441c60dcdb695ef8e9a616e902
noah edward hall <noahedwardhall@gmail.com>
2023-05-29 00:16:30 -0500

    Nirvlife: structure round 1 (#17)
    * Chore(dev): make react template/dev.tsx async
    
    * Feat(core): fork and verify react-fullerpage (#18)

--- apps/nirvlife/dev.tsx
+++ apps/nirvlife/dev.tsx
@@ -1,34 +1,38 @@
 import * as path from "path";
-import { statSync } from "fs";
 import type { ServeOptions } from "bun";
+import { rm, stat } from "node:fs/promises";
 
 const PROJECT_ROOT = import.meta.dir;
 const PUBLIC_DIR = path.resolve(PROJECT_ROOT, "public");
 const BUILD_DIR = path.resolve(PROJECT_ROOT, "build");
 
-await Bun.build({
-  entrypoints: ["./src/main.tsx"],
-  outdir: "./build",
-})
-  .then((output) => {
-    console.info("\n\n built output", output);
+// TODO(noah): see if theres a `bun` way to do this
+// ^ i think bun accepts all the esbuild options so there should be
+await rm(BUILD_DIR, { force: true, recursive: true }).then(() =>
+  Bun.build({
+    entrypoints: ["./src/main.tsx"],
+    outdir: BUILD_DIR,
   })
-  .catch((e) => {
-    console.info("\n\n error in build", e);
-  });
+    .then((output) => {
+      console.info("\n\n built output", output);
+    })
+    .catch((e) => {
+      console.info("\n\n error in build", e);
+    })
+);
 
-function serveFromDir(config: {
+async function serveFromDir(config: {
   directory: string;
   path: string;
-}): Response | null {
+}): Promise<Response | null> {
   let basePath = path.join(config.directory, config.path);
   const suffixes = ["", ".html", "index.html"];
 
   for (const suffix of suffixes) {
     try {
       const pathWithSuffix = path.join(basePath, suffix);
-      const stat = statSync(pathWithSuffix);
-      if (stat && stat.isFile()) {
+      const fd = await stat(pathWithSuffix);
+      if (fd && fd.isFile()) {
         return new Response(Bun.file(pathWithSuffix));
       }
     } catch (err) {}
@@ -38,20 +42,23 @@ function serveFromDir(config: {
 }
 
 export default {
-  fetch(request) {
+  async fetch(request) {
     let reqPath = new URL(request.url).pathname;
     console.log(request.method, reqPath);
     if (reqPath === "/") reqPath = "/index.html";
 
     // check public
-    const publicResponse = serveFromDir({
+    const publicResponse = await serveFromDir({
       directory: PUBLIC_DIR,
       path: reqPath,
     });
     if (publicResponse) return publicResponse;
 
     // check /.build
-    const buildResponse = serveFromDir({ directory: BUILD_DIR, path: reqPath });
+    const buildResponse = await serveFromDir({
+      directory: BUILD_DIR,
+      path: reqPath,
+    });
     if (buildResponse) return buildResponse;
 
     return new Response("File not found", {
