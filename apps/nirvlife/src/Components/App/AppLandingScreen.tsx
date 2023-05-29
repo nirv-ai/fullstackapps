7ae599bf9e3e0b5f8f509636ddcad47b48e6398f
noah edward hall <noahedwardhall@gmail.com>
2023-05-29 00:10:31 -0500

    Feat(core): fork and verify react-fullerpage (#18)

--- apps/nirvlife/src/Components/App/AppLandingScreen.tsx
+++ apps/nirvlife/src/Components/App/AppLandingScreen.tsx
@@ -2,18 +2,51 @@ import { useTheme } from "@mui/material";
 
 import { Img, TextCopy } from "Library";
 import { ctaStacks, nirvaiRef, taglineRef } from "data";
+import { ReactFP, FPContainer, FPItem } from "react-fullerpage";
 // <img src={"/img/logo_main.png"} className="img" alt="nirvai" />
 
 const screenId = "app-landing-screen";
 export const AppLandingScreen = () => {
   const theme = useTheme();
 
+  // {/* <Img key="img" uris={["/img/girljumping.jpg"]} width={500} height={500} /> */}
   return (
-    <article id={screenId}>
-      {/* <Img key="img" uris={["/img/girljumping.jpg"]} width={500} height={500} /> */}
-      <TextCopy data={ctaStacks[0][0]} />
-      <TextCopy data={nirvaiRef} longform />
-      <TextCopy data={taglineRef} full />
-    </article>
+    <ReactFP style={{}}>
+      <FPContainer style={{}} transitionTiming={700}>
+        <FPItem
+          style={{
+            backgroundColor: "lime",
+            height: "80vh",
+            padding: "1em",
+          }}
+        >
+          <article>
+            <TextCopy data={ctaStacks[0][0]} />
+          </article>
+        </FPItem>
+
+        <FPItem
+          style={{
+            backgroundColor: "coral",
+            padding: "1em",
+          }}
+        >
+          <article>
+            <TextCopy data={nirvaiRef} longform />
+          </article>
+        </FPItem>
+
+        <FPItem
+          style={{
+            backgroundColor: "firebrick",
+            padding: "1em",
+          }}
+        >
+          <article>
+            <TextCopy data={taglineRef} full />
+          </article>
+        </FPItem>
+      </FPContainer>
+    </ReactFP>
   );
 };
