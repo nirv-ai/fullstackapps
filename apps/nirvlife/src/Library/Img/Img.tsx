5e91b97e5401f5953897fb89d70c5145ab590dc8
poop <noahedwardhall@gmail.com>
2023-06-04 11:28:36 -0500

    finalize react-idealer-image

--- apps/nirvlife/src/Library/Img/Img.tsx
+++ apps/nirvlife/src/Library/Img/Img.tsx
@@ -1,6 +1,8 @@
 import type { FC } from "react";
 
 import { IdealImage } from "react-idealer-image";
+// import { IdealImage } from "../../../node_modules/react-ideal-image/src";
+
 import { useMemo, useEffect, useState } from "react";
 
 export interface ImgInterface {
